21665cad77cdd500ed5dd988c3d51e4c0456d94e
Brittany Stover <brittany.stover@technekes.com>
2017-01-13 09:30:30 -0500

    adds put method for uploading files to s3

--- lib/s3_file_handler.rb
+++ lib/s3_file_handler.rb
@@ -2,12 +2,17 @@ require 's3_file_handler/version'
 require 'aws-sdk'
 
 class S3FileHandler
-  attr_reader :s3, :options
+  attr_reader :s3, :options, :file_object
   delegate :body, to: :get
 
-  def initialize(bucket, file_key, region = 'us-east-1')
+  def initialize(bucket, file_key, region = 'us-east-1', file_object = nil)
     @s3 = Aws::S3::Client.new(region: region)
     @options = { bucket: bucket, key: file_key }
+    @file_object = file_object
+  end
+
+  def put
+    s3.put_object(options, body: file_object)
   end
 
   def get(path_or_io = nil)
